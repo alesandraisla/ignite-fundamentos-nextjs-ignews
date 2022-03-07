7e80b4211075515655e68a1bb48125978a087d80
AlesandraIsla <alesandramempis@gmail.com>
2022-03-06 21:50:58 -0300

    Static Side generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetServerSideProps} from 'next';
+import { GetStaticProps} from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
@@ -32,7 +32,7 @@ export default function Home({ product } : HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 const price = await stripe.prices.retrieve('price_1KaOmbIM8W3yFskUAegbnCiE', {
   expand: ['product']
 })
@@ -48,6 +48,9 @@ const product = {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+    // quanto tempo em segundos quer que a pagina se mantenha sem precisar
+    // ser revalidada (reconstruida)
   }
 }
